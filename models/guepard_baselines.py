d545f691d47125e0edb1e26ad8693b6d0b732fdd
Kenza Tazi <ktazi100@yahoo.co.uk>
2024-03-15 16:41:36 +0000

    working model version

--- models/guepard_baselines.py
+++ models/guepard_baselines.py
@@ -223,42 +223,43 @@ class latent_GPEnsemble(GPModel, metaclass=abc.ABCMeta):
     
     @property
     def variational_variables(self): # type: ignore
-        q_mus = []
-        q_sqrts = []
+        r = []
         for model in self.models:
-            q_mus.append(model.q_mu)
-            q_sqrts.append(model.q_sqrt)
-        return [q_mus, q_sqrts]
-    
+            r.append([model.q_mu, model.q_sqrt])
+        return r
     
     def maximum_log_likelihood_objective(self, data: List[RegressionData]) -> tf.Tensor:  # type: ignore
+        """ This give a scalar value, data is a zipped list of X and Y """
+        
         [
             isinstance(m, gpflow.models.ExternalDataTrainingLossMixin)
             for m in self.models
         ]
         objectives = [m.training_loss(d) for m, d in zip_longest(self.models, data)]
         return tf.reduce_sum(objectives)
+    
 
     def training_loss(self, Xl, Yl) -> tf.function:
-        """ data is a zipped list of X and Y """
+        """ Return a function """
 
         objectives=[]
 
-        for i in range(len(self.models)):
-            external = isinstance(self.models[i], gpflow.models.ExternalDataTrainingLossMixin)
-
-            if external is False:
-                obj = self.models[i].training_loss()
-            if external is True:
-                obj = self.models[i].training_loss((Xl[i],Yl[i]))
-            objectives.append(obj)
-        
         def closure() -> tf.Tensor:
+
+            for i in range(len(self.models)):
+                external = isinstance(self.models[i], gpflow.models.ExternalDataTrainingLossMixin)
+
+                if external is False:
+                    obj = self.models[i].training_loss()
+                if external is True:
+                    obj = self.models[i].training_loss((Xl[i],Yl[i]))
+                objectives.append(obj)
+        
             return tf.reduce_sum(objectives)
 
         if compile:
             closure = tf.function(closure)
-        return closure
+        return  closure
 
 class Ensemble(GPEnsemble):
     """
@@ -463,62 +464,3 @@ class latent_Ensemble(latent_GPEnsemble):
 
         m, v = self.predict_f(Xnew)
         return self.likelihood.predict_mean_and_var(Xnew, m, v)
-
-
-
-class NestedGP(GPEnsemble):
-    """
-    Implements the Nested GP predictor (aka NPAE).
-    """
-
-    @check_shapes(
-        "Xnew: [N, D]",
-        "return[0]: [N, broadcast L]",
-        "return[1]: [N, broadcast L]",
-    )
-    def predict_f(
-        self, Xnew: InputData, full_cov: bool = False, full_output_cov: bool = False
-    ) -> MeanAndVariance:
-
-        assert not full_output_cov
-
-        X = tf.concat([m.data[0] for m in self.models], axis=0)  # [n, d]
-        Y = tf.concat([m.data[1] for m in self.models], axis=0)  # [n, 1]
-
-        ki_list = [
-            self.kernel(Xnew, m.data[0]) for m in self.models
-        ]  # elements are [q, ni]
-        Ki_list = [
-            self.kernel(m.data[0])
-            + self.likelihood.variance * tf.eye(m.data[0].shape[0], dtype=tf.float64)
-            for m in self.models
-        ]  # elements are [ni, ni]
-
-        Alpha_list = [
-            ki @ tf.linalg.inv(Ki) for ki, Ki in zip(ki_list, Ki_list)
-        ]  # elements are [q, ni]
-        Alpha_ops = [
-            tf.linalg.LinearOperatorFullMatrix(a[:, None, :]) for a in Alpha_list
-        ]
-        Alpha = tf.linalg.LinearOperatorBlockDiag(Alpha_ops)  # [q, p, n]
-
-        Mx = Alpha.matmul(Y)  # [q, p, 1]
-
-        kM_list = [
-            tf.reduce_sum(alpha * ki, axis=1, keepdims=True)
-            for alpha, ki in zip(Alpha_list, ki_list)
-        ]  # elements are [q, 1]
-        kM = tf.stack(kM_list, axis=2)  # [q, 1, p]
-
-        K = tf.expand_dims(self.kernel(X), axis=0)
-        AM = Alpha.to_dense()
-        KM = AM @ K @ tf.transpose(AM, perm=[0, 2, 1])
-        # KM = Alpha.matmul(Alpha.matmul(K), adjoint_arg=True)  # Should be more efficient numerically, but does not return the right values at the moment!
-
-        Alpha2 = kM @ tf.linalg.inv(KM)  # [q, 1, p]
-        mean = Alpha2 @ Mx  # [q, 1, 1]
-
-        var_correction = Alpha2 @ tf.transpose(kM, perm=[0, 2, 1])  # [q, 1, 1]
-        var = self.kernel.K_diag(Xnew)[:, None] - var_correction[:, :, 0]  # [q, 1]
-
-        return mean[:, 0, :], var
