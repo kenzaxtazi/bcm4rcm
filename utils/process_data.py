a74fa6bc9e4238ac404efd82a00aa1bbc249c155
Kenza Tazi <ktazi100@yahoo.co.uk>
2024-11-28 17:39:03 +0000

    updated model list

--- utils/process_data.py
+++ utils/process_data.py
@@ -11,8 +11,8 @@ from load import cordex
 domain = 'WAS'
 model_dict = {'CanESM2':['CanESM2', 'RegCM4'], 'NOAA_IITM':['NOAA', 'RegCM4'], 'CNRM_RegCM4': ['CNRM', 'RegCM4'], 
               'MPI_IITM': ['MPI', 'RegCM4'], 'IPSL_RegCM4': ['IPSL', 'RegCM4'], 'CSIRO': ['CSIRO', 'RegCM4'],
-              'EC-EARTH':['EC-EARTH', 'RCA4'], 'MIROC': ['MIROC','RCA4'], 'NOAA_SMHI':['NOAA', 'RCA4'],
-              'CNRM': ['CNRM', 'RCA4'], 'MPI_RCA4': ['MPI', 'RCA4'], 'IPSL_RCA4': ['IPSL', 'RCA4'],
+              'EC-EARTH':['ECEARTH', 'RCA4'], 'MIROC': ['MIROC','RCA4'], 'NOAA_SMHI':['NOAA', 'RCA4'],
+              'CNRM_RCA4': ['CNRM', 'RCA4'], 'MPI_RCA4': ['MPI', 'RCA4'], 'IPSL_RCA4': ['IPSL', 'RCA4'],
               'MPI_REMO2009':['MPI', 'REMO2009']}
 
 
@@ -25,8 +25,9 @@ def process_data(model_list: list, experiment: str, minyear: str, maxyear: str):
         experiment (str): _description_
     """
     for model in model_list:
-        model_ds = cordex.collect_CORDEX(domain, minyear, maxyear, experiment, 
+        model_ds = cordex.collect_CORDEX(domain, minyear=minyear, maxyear=maxyear, experiment=experiment, 
                                          gcm_model=model_dict[model][0], rcm_model=model_dict[model][1])
+        #print(model_ds)
         sliced_ds = model_ds.sel(lat=slice(20, 40), lon=slice(60, 105))
         sliced_ds = sliced_ds.drop_vars('time_bnds') 
         #print(sliced_ds)
@@ -37,4 +38,6 @@ def process_data(model_list: list, experiment: str, minyear: str, maxyear: str):
         else:
             clean_ds = sliced_ds
         df = clean_ds.to_dataframe().reset_index()
-        df.to_csv('/Users/kenzatazi/Documents/CDT/Code/bcm4rcm/data/processed/'+ experiment + '_' + model + '_' + minyear + '_' + maxyear + '.csv')
+        df.to_csv('/Users/kenzatazi/Documents/CDT/Code/bcm4rcm/data/processed/'+ experiment + '_' 
+                   + model_dict[model][0] + '_' + model_dict[model][1] + '_'
+                   + minyear + '_' + maxyear + '.csv')
